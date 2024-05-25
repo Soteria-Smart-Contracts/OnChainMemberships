878422c188f5abf00ba1c2f3702f14c47399e040
Darius Tanz <scratchticketseb@gmail.com>
2024-05-25 19:23:28 +0000

    May 25, 2024, 3:23 PM

--- Membership.sol
+++ Membership.sol
@@ -76,7 +76,7 @@ contract LiquidSubscription {
         require(msg.value == MembershipLengthPrices[_MembershipLength], "Incorrect amount sent");
         require(MembershipTypes[_MembershipType] <= HighestTypeInt, "Membership type is too high");
 
-        //calculate the time the subscription will expire as
+        //calculate the time the subscription will expire as well as its cost using 
 
 
         SubsctiptionInfo memory _SubscriptionInfo = SubsctiptionInfo({
