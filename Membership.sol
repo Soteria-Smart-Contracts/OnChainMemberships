d3360d900b5c72e33d89f3e14e1cffdaee5f0250
Darius Tanz <scratchticketseb@gmail.com>
2024-05-25 20:39:38 +0000

    May 25, 2024, 4:39 PM

--- Membership.sol
+++ Membership.sol
@@ -103,7 +103,7 @@ contract LiquidSubscription {
         return TokenID;
     }
 
-    function RenewSubscription(uint256 SubscriptionID, uint256 Weeks) public payable{
+    function RenewSubscription(uint256 SubscriptionID, uint256 Weeks) public payable returns(bool success){
         require(Subscriptions[SubscriptionID].SubscriptionExpiry < block.timestamp, "Subscription has not expired yet");
 
         uint256 Discount = GetDiscountEligibility(Weeks);
