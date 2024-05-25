00d4ed21b75b6fdfb944ba61017e169bd11f8810
Darius Tanz <scratchticketseb@gmail.com>
2024-05-25 19:44:18 +0000

    May 25, 2024, 3:44 PM

--- Membership.sol
+++ Membership.sol
@@ -82,6 +82,8 @@ contract LiquidSubscription {
         uint256 Price = MembershipTypes[_MembershipType].BasePrice * Weeks;
         Price = Price - (Price * Discount / 10000);
         require(msg.value >= Price, "Incorrect amount sent");
+
+        uint256 NewExp
         
 
         SubsctiptionInfo memory _SubscriptionInfo = SubsctiptionInfo({
