9901f8e63400bb4cfb2c8c154664b5b27e068730
Darius Tanz <scratchticketseb@gmail.com>
2024-05-25 17:59:50 +0000

    May 25, 2024, 1:59 PM

--- Membership.sol
+++ Membership.sol
@@ -102,7 +102,7 @@ contract LiquidSubscription {
     //View Functions
 
     //function get minimum time for discount
-    function IsEligibleForDiscount(uint256 ) public view returns(DiscountStep memory){
+    function IsEligibleForDiscount(uint256 timebought) public view returns(DiscountStep memory){
         return DiscountSteps[0];
     }
 
