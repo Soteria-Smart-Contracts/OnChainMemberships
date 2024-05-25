be4562e70c9ed94d847bec6ee1b84fe0f03cb958
Darius Tanz <scratchticketseb@gmail.com>
2024-05-25 20:16:23 +0000

    May 25, 2024, 4:16 PM

--- Membership.sol
+++ Membership.sol
@@ -105,7 +105,7 @@ contract LiquidSubscription {
         require(msg.value >= Price, "Incorrect amount sent");
 
         Subscriptions[SubscriptionID].LastPurchaser = msg.sender;
-
+        Dis
         Subscriptions[SubscriptionID].SubscriptionExpiry += (Weeks * WeekUnix);
         Subscriptions[SubscriptionID].TotalWeeksSubscribed += Weeks;
     }
