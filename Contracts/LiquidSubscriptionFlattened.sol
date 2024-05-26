d7598956ee4c1810a256a24e03aa5b25f5dbea28
Darius Tanz <scratchticketseb@gmail.com>
2024-05-26 00:53:11 +0000

    May 25, 2024, 8:53 PM

--- Contracts/LiquidSubscriptionFlattened.sol
+++ Contracts/LiquidSubscriptionFlattened.sol
@@ -143,6 +143,7 @@ contract LiquidSubscription {
     //split subscription function, allowing users to split their subscription into two separate subscriptions nfts and send the other to a desired address
     function SplitSubscription(uint256 SubscriptionID, address Recipient) public returns(uint256 NewSubscriptionTokenID){
         require(Recipient != address(0), "Recipient cannot be the zero address");
+        //warning ig 
 
         uint256 TimeLeft = Subscriptions[SubscriptionID].SubscriptionExpiry - block.timestamp;
         uint256 WeeksEquivalent = TimeLeft / WeekUnix;
