0d82880f2292709861a73d4622303f31a3ab6e81
Darius Tanz <scratchticketseb@gmail.com>
2024-05-26 15:20:53 +0000

    May 26, 2024, 11:20 AM

--- Website/src/js/Interaction.js
+++ Website/src/js/Interaction.js
@@ -2,7 +2,7 @@ const contractAddress = "0xf5c9e57e177B4F5CCfCb13b18e4154774E917401";
 const ABI = window.abi;
 let account;
 let netID;
-let LoggedIn = false;
+let goto;
 
 
 async function loginWithEth(goto){
