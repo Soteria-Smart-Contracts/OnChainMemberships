0cca04a1a4412c6e69e0702e9441f988cc0792a3
Darius Tanz <scratchticketseb@gmail.com>
2024-05-26 01:10:09 +0000

    May 25, 2024, 9:10 PM

--- Contracts/LiquidSubscriptionFlattened.sol
+++ Contracts/LiquidSubscriptionFlattened.sol
@@ -146,7 +146,7 @@ contract LiquidSubscription {
         require(Recipient != address(0), "Recipient cannot be the zero address");
         require(Weeks >= 1, "Weeks must be greater than or equal to 1");
         //require the subscription to have atleast one week on it and its not expired
-        require(Subscriptions[SubscriptionID].TotalWeeksSubscribed >= Weeks, "Not enough weeks to split");
+        require(Subscriptions[SubscriptionID].TotalWeeksSubscribed >= Weeks && , "Not enough weeks to split");
         //warning if you have two memberships in the same address, they will both continue to elapse
 
         uint256 TimeLeft = Subscriptions[SubscriptionID].SubscriptionExpiry - block.timestamp;
